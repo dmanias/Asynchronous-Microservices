2d8dba4e9ad94a4680d9f5ed840cc4cddab6a063
Dimosthenis Manias <dimosthenis.manias@gmail.com>
2023-07-25 16:25:17 +0300

    add files

---
+++ kafka-consumer-database/src/main/java/gr/dmanias/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package gr.dmanias.springboot.repository;
+
+import gr.dmanias.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
---
+++ kafka-producer-wikimedia/src/main/java/gr/dmanias/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,23 @@
+package gr.dmanias.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication implements CommandLineRunner {
+
+    private final WikimediaChangesProducer wikimediaChangesProducer;
+
+    public SpringBootProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
+    }
+}
